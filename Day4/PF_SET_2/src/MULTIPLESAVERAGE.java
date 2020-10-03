76e5b9b761d5d901a5a149c3176eb809918160dc
devilnonda <4sf15cs122@sahyadri.edu.in>
2020-10-03 22:25:37 +0530

    PF_SET_2

--- Day4/PF_SET_2/src/MULTIPLESAVERAGE.java
+++ Day4/PF_SET_2/src/MULTIPLESAVERAGE.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
 public class MULTIPLESAVERAGE {
+    int multiple=5;
+    public float findAveragesBy5s(int n){
+
+        int count=n/this.multiple;
+        return (float) (count+1)*5/2;
+    }
 public static void main(String[] args){
-    int multiple=5,n;
+    int n;
     Scanner sc=new Scanner(System.in);
     n=sc.nextInt();
     if(n>0 && n<32767){
-        int count=n/multiple;
-        System.out.format("%.2f",(float) (count+1)*5/2);
+        MULTIPLESAVERAGE ma=new MULTIPLESAVERAGE();
+        System.out.format("%.2f",ma.findAveragesBy5s(n));
     }
     else
         System.out.println("Invalid input");
---
+++ Day4/PF_SET_2/src/SUMOFODDDIGITS.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SUMOFODDDIGITS {
+    public int sumOddDigits(int n) {
+        int count = 0, sum = 0;
+        if (n < 0 || n > 32767) {
+            return -1;
+        }
+        while (n != 0) {
+            int temp = n % 10;
+            if (temp % 2 == 1) {
+                sum += temp;
+            }
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        SUMOFODDDIGITS sodd=new SUMOFODDDIGITS();
+        int output=sodd.sumOddDigits(n);
+        if(output==-1)
+            System.out.println("Invalid input");
+        else
+            System.out.println(output);
+        sc.close();
+    }
+}
