1529175bf938d45a598eb8e23c7d546c6334b5e4
devilnonda <4sf15cs122@sahyadri.edu.in>
2020-10-19 08:56:30 +0530

    Kotlin Assignment

--- ADay_10/Assignment/src/assignment1/12.ExtendSimpleInterest.kt
+++ ADay_10/Assignment/src/assignment1/12.ExtendSimpleInterest.kt
@@ -1,20 +1,21 @@
 package assignment1
 
-fun MutableMap<Int, Int>.simpleInterest(key: Int): Double {
+fun MutableMap<Int, ArrayList<Int>>.simpleInterest(key: Int): Int {
 
-    var p = this[key]?.toInt()
-    if (p != null) {
-        return p * 5.0 * 3 / 100
-    } else
-        return 0.0
+    var p = this[key]
+//print(p?.get(0))
+    //print(p?.get(1))
+    if (p != null) return key * p?.get(0) * p?.get(1) / 100 else
+        return 0
 
 
 }
 
+
 fun main() {
-    var a: MutableMap<Int, Int> = mutableMapOf()
-    a.put(1, 1000)
-    a.put(2, 2000)
+    var a: MutableMap<Int, ArrayList<Int>> = mutableMapOf()
+    a.put(1000, arrayListOf(12,1))
+   // a.put(2, 2000)
     for (i in a) {
         println("principal amount=${i.value} interest=${a.simpleInterest(i.key)}")
     }
